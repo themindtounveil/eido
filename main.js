7d328019e124ca055e89ed9eb874bde728d0b467
themindtounveil <arg.ginosko@gmail.com>
2026-04-10 23:08:26 -0700

    .

--- main.js
+++ main.js
@@ -3,16 +3,13 @@ let openNavigationBar = document.getElementsByClassName("openNavigationBar");
 let topBar = document.getElementsByClassName("topBar");
 let closeNavigationBar = document.getElementsByClassName("closeNavigationBar")
 let eidoHome = document.getElementsByClassName("eidoHome")
+
 openNavigationBar.addEventListener("mouseover", event => {
-    if (size != 0 & size == 8) {
      topBar.style.z-index= 19;
      eidoHome.style.z-index= 19;
-    }
 });
 
 closeNavigationBar.addEventListener("mouseover", event => {
-    if (size == 0 & size != 8) {
      topBar.style.z-index= -9;
      eidoHome.style.z-index = "-9";
-    };
 });
