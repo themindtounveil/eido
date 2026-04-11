58bd51c9faab8e5d39f1a07632be8ba5859734f1
themindtounveil <arg.ginosko@gmail.com>
2026-04-10 22:34:39 -0700

    .

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ openNavigationBar.addEventListener("mouseout", event => {
 
 while(while1=="loop") {
     topBar.style.height=size+"%";
-    if (bar == "closed", size != 0, size == 8) {
+    if (bar == "closed" & size != 0 & size == 8) {
     size--
-    } else if (bar == "open", size == 0, size != 8) {
+    } else if (bar == "open" & size == 0 & size != 8) {
         size++
     };
 };
