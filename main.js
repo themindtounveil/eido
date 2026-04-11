b8653bee14a03baafc61bc3d03893784000e01d5
themindtounveil <arg.ginosko@gmail.com>
2026-04-10 20:30:23 -0700

    navBarT1

---
+++ main.js
@@ -0,0 +1,26 @@
+const openNavigationBar = document.getElementsByClassName("openNavigationBar");
+const topBar = document.getElementsByClassName("top-bar");
+
+
+openNavigationBar.addEventListener("mouseover", event => {
+    bar = "open";
+});
+
+openNavigationBar.addEventListener("mouseout", event => {
+    bar = "closed";
+});
+
+while(while1=="loop") {
+    topBar.style.height=size+"%";
+    if (bar == "closed", size != 0, size == 8) {
+    size--
+    } else if (bar == "open", size == 0, size != 8) {
+        size++
+    };
+};
+
+
+
+    const while1="loop";
+    let bar = "closed";
+    let size = 0;
